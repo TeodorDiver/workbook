574451a5f268c7c4454652340c0e2b93b3f531c6
TeodorDiver <satarovsergej@gmail.com>
2024-09-25 13:34:23 +0400

    task19 finish

--- workbook/task19.cpp
+++ workbook/task19.cpp
@@ -10,12 +10,12 @@ void task19() {
 	std::cout << "- найти номер первой из строк, содержащих хотя бы один положительный элемент\n";
 	std::cout << "РЕШЕНИЕ\n";
 
-	const int n = 18, m = 3;
+	const int n = 3, m = 8;
 	int b[n][m], countN = 0, countM = 0;
 
 	for (int i = 0; i < n; i++)									//создали матрицу
 		for (int j = 0; j < m; j++)
-			b[i][j] = rand() % 2 - 1;
+			b[i][j] = rand() % 3 - 1;
 
 	std::cout << "- вывели матрицу:\n";
 
@@ -27,9 +27,10 @@ void task19() {
 		std::cout << "\n";
 	}
 
-	int x = n;
+	
+	int x = n;											//для уменьшения строк
 
-	for (int i = 0; i < x-1; i++) {
+	for (int i = 0; i < x - 1; i++) {					//цикл для сортировки строк
 		bool rowIsNull = 1;
 		for (int j = 0; j < m; j++) {
 			if (b[i][j] != 0) {
@@ -47,25 +48,59 @@ void task19() {
 					b[k + 1][l] = temp;
 				}
 			x--;
+			countN++;
 		}
 		
-		
-		std::cout << countN <<"- countN\n";
+	}
+
+	int y = m;											//для уменьшения столбцов
+
+	for (int i = 0; i < y; i++) {						//цикл для сортировки столбцов
+		bool columnIsNull = 1;
+		for (int j = 0; j < n; j++) {
+			if (b[j][i] != 0) {
+				columnIsNull = 0;
+				break;
+			}
+
+		}
+
+		if (columnIsNull) {
+			for (int k = i; k < y; k++)
+				for (int l = 0; l < n; l++) {
+					int temp = b[l][k+1];
+					b[l][k] = b[l][k+1];				//столбцы с нулями на правый край матрицы
+					b[l][k+1] = temp;
+				}
+			y--;
+			countM++;
+		}
+
 	}
 	
 
-	std::cout << "- вывели матрицу:\n";
+	std::cout << "- вывели матрицу:\n\n\n\n";		 //в выводе не показываем нулевые строки снизу и справа
 
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < m; j++) {
+		for (int i = 0; i < n - countN; i++) {
+			for (int j = 0; j < m - countM; j++) {
 				std::cout.width(10);
 				std::cout << b[i][j];
 			}
 			std::cout << "\n";
 		}
 
-
-	
+		int z = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++)
+				if (b[i][j] > 0) {
+					z = i + 1;
+					break;
+				}
+			if (z > 0) {
+				std::cout << "\n строка с положительным элементом: " << z;
+				break;
+			}
+		}
 
 
 
